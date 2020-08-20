8535221d3084602cddd49cbc554d08fc6829f14b
Jazmin Matos <jmatos1993@gmail.com>
2020-08-20 19:41:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
-    return "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase != phrase.upcase)
-    return "HUH?! SPEAK UP, SONNY!"
-  else (phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!")
-    return "NO, NOT SINCE 1938!"
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 end
